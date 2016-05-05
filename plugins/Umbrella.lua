be3aac419857fcbcd01eae560217a6a928f0f03c
hjsrzvpqma <makan.j100100@gmail.com>
2016-05-06 03:14:39 +0430

    Update and rename Umbrella-cp.lua to Umbrella.lua

--- plugins/Umbrella.lua
+++ plugins/Umbrella.lua
@@ -22,7 +22,7 @@ local space = '______________________________'
 if not group_link then
 return ''
 end
-return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nMacintosh A9 Procesor\nCPU : 16 Core\nRAM : 32 GB\nHDD : 2 x 4 TB\nIPN : 7 MB/S\nPort : 2 MB/s\n"..space.."\nگیتهاب Umbrella-Cp :\n"..github.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: #CopierTeam\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/CopierTeam\n لینک ساپورت :\n"..group_link
+return "مشحصات فنی سرور\n مدت آنلاین :"..uptime.."\nMacintosh A9 Procesor\nCPU : 16 Core\nRAM : 32 GB\nHDD : 2 x 4 TB\nIPN : 7 MB/S\nPort : 2 MB/s\n"..space.."\n"..github.."\n توسئه دهندگان :\nسازنده و صاحب امتیاز: @MAKAN\n"..admins.."\n"..space.."\nپلهای ارتباطی :\nکانال : https://telegram.me/umbrellateam\n لینک ساپورت :\n"..group_link
 end
 return {
 patterns = {
